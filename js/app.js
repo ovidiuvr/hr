a20f8b83637b3b3d97c1131dc0fc60385f938e62
ovidiuvr <ovidiuv.ritea@gmail.com>
2026-04-19 18:13:55 +0300

    Activare navigare module: dashboard, organizare, personal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
-// Punem ../ pentru a ieși din folderul "js" și a intra în "modules"
+// 1. Importăm funcțiile din modulele noastre
 import { renderDashboard } from '../modules/dashboard.js';
+import { renderOrganizare } from '../modules/organizare.js';
+import { renderPersonal } from '../modules/personal.js';
 
+// 2. Definim funcția nav și o facem globală (window.nav)
+// Astfel, butoanele din index.html o pot găsi
+window.nav = function(pagina) {
+    console.log("Navigăm către: " + pagina);
+
+    // Verificăm ce pagină a fost cerută și apelăm funcția corespunzătoare
+    if (pagina === 'dashboard') {
+        renderDashboard();
+    } else if (pagina === 'organizare') {
+        renderOrganizare();
+    } else if (pagina === 'personal') {
+        renderPersonal();
+    }
+};
+
+// 3. Afișăm Dashboard-ul automat când se deschide aplicația
 console.log("Aplicația HR PRO a pornit! 🚀");
-
 renderDashboard();

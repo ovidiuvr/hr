3ed096b837601583aeeea64b0b05d05feeec1ce0
ovidiuvr <ovidiuv.ritea@gmail.com>
2026-04-19 16:57:17 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-import { router } from './tool.js';
+// Schimbăm de la '../tool.js' la '/tool.js' (cu slash în față)
+import { router } from '/tool.js';
+import { renderDashboard } from '/modules/dashboard.js';
 
 window.nav = function(pagina) {
     router(pagina);

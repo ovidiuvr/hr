852d78c245eae16078a8370d2baa3b515416555f
ovidiuvr <ovidiuv.ritea@gmail.com>
2026-04-19 17:54:41 +0300

    Versiune Stabilă: Corectare cale import dashboard

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
-// Importăm funcția folosind o cale relativă pentru compatibilitate cu Render
-import { renderDashboard } from './modules/dashboard.js';
+// Punem ../ pentru a ieși din folderul "js" și a intra în "modules"
+import { renderDashboard } from '../modules/dashboard.js';
 
 console.log("Aplicația HR PRO a pornit! 🚀");
 
-// Pornim interfața
 renderDashboard();
